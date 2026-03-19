0c25937cb5bfeae92f2b3ebd1886878c71faf8d8
HuNteR0i <krystian.kaminski325mok@gmail.com>
2026-03-19 10:55:18 +0100

    spam fix

--- contackt.js
+++ contackt.js
@@ -1,16 +1,22 @@
 document.getElementById("contact-form").addEventListener("submit", function(event) {
-  event.preventDefault();
+    event.preventDefault();
+  
+    emailjs.sendForm(
+        "service_f2kot8m",   // your EmailJS service ID
+        "template_qmvfkx9",  // your template ID
+        this
+    ).then(
+        function() {
+            alert("Email sent successfully!"); 
+            setTimeout(function(){
+                window.location.href = "index.html";
+            }, 3000);
+            
 
-  emailjs.sendForm(
-      "service_f2kot8m",   // your EmailJS service ID
-      "template_qmvfkx9",  // your template ID
-      this
-  ).then(
-      function() {
-          alert("Email sent successfully!");
-      },
-      function(error) {
-          alert("Failed to send email: " + error.text);
-      }
-  );
-});
+        },
+        function(error) {
+            console.error("Failed to send email:", error);
+            alert("Failed to send email: " + error.text);
+        }
+    );
+  });
